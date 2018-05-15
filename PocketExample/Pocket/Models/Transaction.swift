0bf06b584ec64419b4d957590dd94c08048c7f2b
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2018-05-15 09:55:05 -0400

    Updated docs and moved files around

--- PocketExample/Pocket/Models/Transaction.swift
+++ PocketExample/Pocket/Models/Transaction.swift
@@ -16,35 +16,19 @@ public struct Transaction {
     public var signed = ""
 
     /**
-     
      Signs an unsigned Transaction type object.
-     
-     Example:
-     
+
+     ```
+     // Example: Signs a transaction.
      let transaction = Transaction(nonce: "noncenumber", recipient: "recipient'saddress", value: "0.0", data: [key:value], signed: nil)
-     
      let signedTransaction = signTransaction(transaction: transaction)
+     ```
+
+     - Parameters:
+        - transaction: Unsigned Transaction type object.
      
-     Signs a transaction
-     
-     - **Returns**:
-     
-        A Signed transaction type object
-     
-     - **Parameters**:
-     
-        - transaction: Unsigned Transaction type object. Can not be empty.
-     
-     - **Important**:
-     
-        Draft method.
-     
-     - **Version**:
-     
-        0.1
-     
+     - Returns: A Signed transaction type object.
      */
-    
     public mutating func signTransaction(transaction: Transaction) {
         // Sign transaction
         let signedTransaction = ""
--- PocketExample/Pocket/Models/Wallet.swift
+++ PocketExample/Pocket/Models/Wallet.swift
@@ -14,88 +14,44 @@ public struct Wallet {
     public var network: Network?
     
     /**
-     
      Creates a wallet using a string for an especific network
-     
-     Example:
-     
+
+     ```
+     // Example: Creates a new Wallet for Ethereum network.
      let passphrase = "thisisapasphrase"
-     
      let network = Network(tokenID: "ETH")
-     
      let wallet = createWalletWith(passphrase: passphrase, network: network)
-     
-     Creates a new Wallet for Ethereum network
-     
-     - **Returns**:
-     
-        A Wallet type object
-     
-     - **Parameters**:
-     
+     ```
+
+     - Parameters:
         - passphrase: Passphrase (string) to create the Wallet. Can not be empty.
-     
         - network: Network type. Can not be empty.
-     
-     - **Important**:
-     
-        Draft method.
-     
-     - **Version**:
-     
-        0.1
-     
-     Uses a passphrase string and a network as parameters to create a wallet
-     
-     for an especific network.
-     
+
+     - Returns: A Wallet type object
      */
-    
-    public func createWalletWith(passphrase: String, network: Network) -> Wallet {
-        // Create Wallet with passphrase
+    public func createWallet(withPassphrase passphrase: String, network: Network) -> Wallet {
         return Wallet(address: "", privateKey: "", network: Network(tokenID: ""))
     }
     
     /**
-     
-     Imports a wallet using a private key
-     
-     Example:
-     
+     Imports a wallet using a private key.
+
+     ```
+     // Example: Imports an Ethereum Wallet.
      let privateKey = "longprivatekeystring"
      let address = "0xwalletaddress"
      let network = Network(tokenID: "ETH")
-     
      let wallet = importWallet(walletKey: privateKey, address: address, network: network)
-     
-     Imports a Ethereum Wallet
-     
-     - **Returns**:
-     
-        A Wallet type object
-     
-     - **Parameters**:
-     
-        - walletKey: Private key (string) to import the Wallet. Can not be empty.
-     
-        - address: Address related to the Private key (string). Can not be empty.
-     
+     ```
+
+     - Parameters:
+        - key: Private key (string) to import the Wallet.
+        - address: Address related to the Private key (string).
         - network: Network type. Can not be empty.
-     
-     - **Important**:
-     
-        Draft method.
-     
-     - **Version**:
-     
-        0.1
-     
-     Uses a private key string to import a wallet for an especific network.
-     
+
+     - Returns: A Wallet type object
      */
-    
-    public func importWallet(walletKey: String, address: String, network: Network) -> Wallet {
-        // Create Wallet from private key
+    public func importWallet(withKey key: String, address: String, network: Network) -> Wallet {
         return Wallet(address: "", privateKey: "", network: Network(tokenID: ""))
     }
 }

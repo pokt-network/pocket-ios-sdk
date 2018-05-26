0812651c5af67f69ec68f5d51528cacd08c78fe8
Michael O'Rourke <>
2018-05-26 13:31:27 -0300

    added questing VCs

--- Example/ViewControllers/CompleteQuest/PlayerMapViewController.swift
+++ Example/ViewControllers/CompleteQuest/PlayerMapViewController.swift
@@ -12,48 +12,48 @@ import MapKit
 class PlayerMapViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    
+
     fileprivate let locationManager = CLLocationManager()
     fileprivate var startedLoadingPOIs = false
-    fileprivate var places = [Place]()
-    
+    //fileprivate var places = [Place]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         mapView.showsUserLocation = true
 
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestAlwaysAuthorization()
-        
+
         if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
             locationManager.requestWhenInUseAuthorization()
         }else{
             print("Location services are disabled, please enable before trying again.")
         }
-        
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.count > 0 {
             let location = locations.last!
             print("Accuracy: \(location.horizontalAccuracy)")
-            
+
             //2
             if location.horizontalAccuracy < 100 {
                 //3
@@ -61,7 +61,7 @@ class PlayerMapViewController: UIViewController, CLLocationManagerDelegate {
                 let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
                 let region = MKCoordinateRegion(center: location.coordinate, span: span)
                 mapView.region = region
-                
+
                 if !startedLoadingPOIs {
                     startedLoadingPOIs = true
                     //2
@@ -82,8 +82,8 @@ class PlayerMapViewController: UIViewController, CLLocationManagerDelegate {
             print("Failed to get current location")
         }
     }
-    
-    
+
+
 }
 // MARK: - Extensions
 extension ViewController: CLLocationManagerDelegate {

784a68162ef3e502e63a3d02978f4662a90faee5
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-05-27 01:52:20 -0300

    changes

---
+++ Example/UITableViewCells/ChooseQuestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseQuestTableViewCell.swift
+//  Example
+//
+//  Created by Pabel Nunez Landestoy on 5/27/18.
+//  Copyright © 2018 Pocket Network. All rights reserved.
+//
+
+import UIKit
+
+class ChooseQuestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

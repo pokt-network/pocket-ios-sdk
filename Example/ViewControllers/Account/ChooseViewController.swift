f72ddf5572291ea61d2fe4c08729c78478588009
Michael O'Rourke <>
2018-05-27 01:03:07 -0300

    fixed segue

--- Example/ViewControllers/Account/ChooseViewController.swift
+++ Example/ViewControllers/Account/ChooseViewController.swift
@@ -17,6 +17,15 @@ class ChooseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let configuration = EthAccountConfiguration(namespace: "wallet", password: nil)
+        let (keystore, _) = EthAccountCoordinator.default.launch(configuration)
+        
+        let jsonKey = "{\"version\":3,\"id\":\"af185f3f-e8f3-463f-ad47-1cdcf94086d9\",\"address\":\"e955199873abd97a921f8b57d27809d57bff6329\",\"Crypto\":{\"ciphertext\":\"476ac878ffdb92c5f969c65cba5574ea13ea73045603011770056711d17464ff\",\"cipherparams\":{\"iv\":\"a23b34daeb9f8ba4951e06cdaf2e8f6e\"},\"cipher\":\"aes-128-ctr\",\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"salt\":\"fa26f38cce0e29215bbef32fdc803b28509475f021a3d7a7170ec9d8c34e3d87\",\"n\":8192,\"r\":8,\"p\":1},\"mac\":\"63fe5b45438aa04318984cb816744296aca9b5a7b3621d91b46e89045c065f56\"}}"
+        let currentPassphrase = "123456789"
+        let gethAccount = EthAccountCoordinator.default.importPrivateKey(jsonKey, passphrase: currentPassphrase, newPassphrase: currentPassphrase)
+    
+    
         completeButton.backgroundColor = .clear
         completeButton.layer.cornerRadius = 5
         completeButton.layer.borderWidth = 1

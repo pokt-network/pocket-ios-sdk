89d3ea20e85f0ff6d6c15048306fe6ac05300013
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-06-21 09:48:01 -0400

    Account management (#17)
    * implemented account management in keystore
    
    * Remoed WalletManager
    
    * Refactored Wallet model
    
    * Added RNCryptor / Added encryption/decryption to wallet persistence

--- Pocket/Pocket/Models/Wallet.swift
+++ Pocket/Pocket/Models/Wallet.swift
@@ -7,17 +7,94 @@
 //
 
 import Foundation
+import SwiftKeychainWrapper
+import RNCryptor
+
+public enum WalletPersistenceError: Error {
+    case invalidWallet
+    case walletSerializationError
+}
 
 public struct Wallet {
     public var address = ""
     public var privateKey = ""
     public var network = ""
     public var data:[AnyHashable : Any]? = [AnyHashable : Any]()
+
+    //Public interface
+    public init(jsonString: String) throws {
+        var dict = try jsonStringToDictionary(string: jsonString) ?? [AnyHashable: Any]()
+        address = dict["address"] as? String ?? ""
+        privateKey = dict["privateKey"] as? String ?? ""
+        network = dict["network"] as? String ?? ""
+        data = try jsonStringToDictionary(string: dict["data"] as? String ?? "")
+    }
     
     public init(address: String, privateKey: String, network: String, data: [AnyHashable : Any]?) {
         self.address = address
         self.privateKey = privateKey
         self.network = network
         self.data = data
+    }
+    
+    // Persistence public interface
+    public func save(passphrase: String) throws -> Bool {
+        if isValid() {
+            guard let jsonData = try toJSONString().data(using: .utf8) else {
+                throw WalletPersistenceError.invalidWallet
+            }
+            let ciphertext = RNCryptor.encrypt(data: jsonData, withPassword: passphrase)
+            return  KeychainWrapper.standard.set(ciphertext, forKey: recordKey())
+        } else {
+            throw WalletPersistenceError.invalidWallet
+        }
+    }
+    
+    public func delete() throws -> Bool {
+        return KeychainWrapper.standard.removeObject(forKey: recordKey())
+    }
+    
+    public static func retrieveWalletRecordKeys() -> [String] {
+        return Array(KeychainWrapper.standard.allKeys())
+    }
+    
+    public static func retrieveWallet(network: String, address: String, passphrase: String) throws -> Wallet{
+        guard let encryptedWalletString:String = KeychainWrapper.standard.string(forKey: Wallet.recordKey(network: network, address: address)) else {
+            throw WalletPersistenceError.walletSerializationError
+        }
+        guard let encryptedWalletData = encryptedWalletString.data(using: .utf8) else {
+            throw WalletPersistenceError.walletSerializationError
+        }
+        guard let decryptedWalletJSON = String.init(data: try RNCryptor.decrypt(data: encryptedWalletData, withPassword: passphrase), encoding: .utf8) else {
+            throw WalletPersistenceError.walletSerializationError
+        }
+        let wallet = try Wallet(jsonString: decryptedWalletJSON)
+        return wallet
+    }
+    
+    // Private functions
+    private func isValid() -> Bool {
+        return !network.isEmpty && !address.isEmpty && !privateKey.isEmpty
+    }
+    
+    private func recordKey() -> String {
+        return Wallet.recordKey(network: network, address: address)
+    }
+    
+    private static func recordKey(network: String, address: String) -> String {
+        return network + "/" + address
+    }
+    
+    private func toJSONString() throws -> String {
+        var object = [AnyHashable: Any]()
+        object["address"] = address
+        object["privateKey"] = privateKey
+        object["network"] = network
+        object["data"] = try dictionaryToJsonString(dict: data)
+        guard let result = try dictionaryToJsonString(dict: object) else {
+            throw WalletPersistenceError.walletSerializationError
+        }
+        
+        return result
     }
 }

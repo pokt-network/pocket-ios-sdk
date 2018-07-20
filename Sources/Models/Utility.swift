40df7b6ee5ee9db3ec326b0a5df51b60b3af8b99
Luis de Leon <luyzdeleon@users.noreply.github.com>
2018-07-20 13:01:32 -0400

    Fixed queryresponse result (#27)
    Approved

--- Sources/Models/Utility.swift
+++ Sources/Models/Utility.swift
@@ -21,10 +21,28 @@ public struct Utility {
 
         return rawData
     }
+    
+    public static func jsonStringToArray(string: String) throws -> [Any]? {
+        guard let object = string.data(using: .utf8, allowLossyConversion: false) else {
+            throw NSError()
+        }
+        
+        guard let rawData = try JSONSerialization.jsonObject(with: object, options: .allowFragments) as? [Any] else {
+            throw NSError()
+        }
+        
+        return rawData
+    }
 
     public static func dictionaryToJsonString(dict: [AnyHashable: Any]?) throws -> String? {
         let object = dict ?? [AnyHashable: Any]()
         let data = try JSONSerialization.data(withJSONObject: object, options: .sortedKeys)
+        return String(data: data, encoding: .utf8)
+    }
+    
+    public static func arrayToJsonString(arr: [Any]?) throws -> String? {
+        let array = arr ?? [Any]()
+        let data = try JSONSerialization.data(withJSONObject: array, options: .sortedKeys)
         return String(data: data, encoding: .utf8)
     }
 
